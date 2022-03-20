e823dad2386a38853d4cd3be91e95ff2433fa7b9
Rasoul Abdulzahra <rasoul.abdulzahra@aubay.it>
2022-03-20 11:49:00 +0100

    configured db connection

--- db/init.sql
+++ db/init.sql
@@ -7,6 +7,14 @@ CREATE TABLE Student(
 INSERT INTO Student(student_name, student_age) VALUES("Shubham verma", 21);
 INSERT INTO Student(student_name, student_age) VALUES("Utkarsh verma", 23);
 
-ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'password';
+CREATE TABLE Project(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	title VARCHAR(255),
+);
+
+INSERT INTO Project(title) VALUES("project 1");
+INSERT INTO Project(title) VALUES("project 2");
+
+CREATE USER 'root@localhost' IDENTIFIED WITH mysql_native_password BY 'password';
 
 flush privileges;
