40ca3733ad9dfea5672412f217b264d0a3f91393
Rasoul Abdulzahra <rasoul.abdulzahra@aubay.it>
2022-03-23 12:15:32 +0100

    implemented text serach

--- client/src/pages/Projects/index.tsx
+++ client/src/pages/Projects/index.tsx
@@ -1,9 +1,31 @@
+import {useState} from "react";
 import {useQuery} from "react-query";
 
 const Projects = () => {
 	const {data, isLoading, error} = useQuery("projects", () =>
 		fetch("http://localhost:8080/projects/").then((res) => res.json())
 	);
+	const [title, setTitle] = useState("");
+	const [foundUsers, setFoundUsers] = useState(data.projects);
+
+	const filter = (e: {target: {value: any}}) => {
+		const keyword = e.target.value;
+
+		if (keyword !== "") {
+			const results = data.projects.filter((project: {title: string}) => {
+				return project.title
+					.toLowerCase()
+					.startsWith(keyword.toLowerCase());
+				// Use the toLowerCase() method to make it case-insensitive
+			});
+			setFoundUsers(results);
+		} else {
+			setFoundUsers(data.projects);
+			// If the text field is empty, show all projects
+		}
+
+		setTitle(keyword);
+	};
 
 	if (isLoading) {
 		return <div>Loading...</div>;
@@ -12,12 +34,19 @@ const Projects = () => {
 		return <div>Error!</div>;
 	}
 
-	console.table(data);
+	console.table(foundUsers);
 	return (
 		<div>
 			<h1>Projects</h1>
+			<input
+				type='search'
+				value={title}
+				onChange={filter}
+				className='input'
+				placeholder='Filter'
+			/>
 
-			<pre>{JSON.stringify(data, null, 2)}</pre>
+			<pre>{JSON.stringify(foundUsers, null, 2)}</pre>
 		</div>
 	);
 };
